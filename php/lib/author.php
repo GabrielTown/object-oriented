6ea619f66c2b2faf6d002cad8d7d1b6b804d1abc
Gabriel Town <rieltown@gmail.com>
2020-01-28 16:03:44 -0700

    got rid of extra echo statements addded .

--- php/lib/author.php
+++ php/lib/author.php
@@ -7,15 +7,8 @@ $hash = password_hash("password", PASSWORD_ARGON2I, ["time_cost" => 7]);
 //var_dump($hash);
 $oliverKlozoff = new Author('6145f31f-8a8f-4492-baf2-ba1082ecc0ef', '66941404092275938727437995023979 ', 'https://www.twitter.com', 'oklozoff@gmail.com', $hash, 'klozoff' );
 
-echo "Author Id:  {$oliverKlozoff->getAuthorId()}";
-echo "<br>";
-echo "Author Activation Hash: {$oliverKlozoff->getAuthorActivationToken()}";
-echo "<br>";
-echo "Author Avatar Url: {$oliverKlozoff->getAuthorAvatarUrl()}";
-echo "<br>";
-echo "Author Email: {$oliverKlozoff->getAuthorEmail()}";
-echo "<br>";
-echo "Author Hash: {$oliverKlozoff->getAuthorHash()}";
-echo "<br>";
-echo "Author Username: {$oliverKlozoff->getAuthorUsername()}";
+echo "Author Id:  {$oliverKlozoff->getAuthorId()}" . '<br>' . "Author Activation Hash: {$oliverKlozoff->getAuthorActivationToken()}"
+		. '<br>' . "Author Avatar Url: {$oliverKlozoff->getAuthorAvatarUrl()}" . '<br>' . "Author Email: {$oliverKlozoff->getAuthorEmail()}"
+		. '<br>' . "Author Hash: {$oliverKlozoff->getAuthorHash()}" . '<br>' . "Author Username: {$oliverKlozoff->getAuthorUsername()}";
+
 
